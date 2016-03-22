2d1a62607bff3aa2e3a4c5994776eb6e762692fc
Christopher Papke <kordex0@gmail.com>
2016-03-22 08:39:55 -0700

    julia factors

--- julia/factors.jl
+++ julia/factors.jl
@@ -1,12 +1,31 @@
 
-function primeFactorSet(n::Int)
-    s = Set()
-    if(n % 2 == 0)
-        push!(s, 2)
-        n = div(n, 2)
-        while n % 2 == 0
-        
+@inline function primeFactorSetPartial(s::Set{Int}, n::Int, i::Int)
+    if(n % i == 0)
+        push!(s, i)
+        n = div(n, i)
+        while n % i == 0
+            n = div(n, i)
         end
     end
+    return n
+end
+
+function primeFactorSet(n::Int)
+    s = Set{Int}()
+    n = primeFactorSetPartial(s, n, 2)
+    n = primeFactorSetPartial(s, n, 3)
+    
+    i = 5
+    while i <= n
+        if i*i > n
+            push!(s, n)
+            n = 1
+            break
+        end
+        n = primeFactorSetPartial(s, n, i)
+        n = primeFactorSetPartial(s, n, i+2)
+        i += 6
+    end
+    return s
 end
 
