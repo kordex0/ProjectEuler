09ecd075dc8f673141c57e5fb5332276d8ff37ed
Christopher Papke <kordex0@gmail.com>
2016-04-05 23:02:37 -0700

    FINISHED 421!
    also fixed stupid factor error

--- julia/factors.jl
+++ julia/factors.jl
@@ -32,3 +32,20 @@ function primeFactorSet{T}(n::T)
     return s
 end
 
+function primeFactorSet{T}(n::T, primes::Array{T})
+    s = Set{T}()
+    for p in primes
+        if p*p > n
+            if n > 1
+                push!(s, n)
+            end
+            break
+        end
+        n = primeFactorSetPartial(s, n, p)
+        if n == 1
+            break
+        end
+    end
+    return s
+end
+
--- julia/problem421.jl
+++ julia/problem421.jl
@@ -1,149 +1,67 @@
 
 using Benchmarks
-
-import Base.div
+using ProfileView
 
 include("primes.jl")
+include("factors.jl")
 
-#Berlekamp's algorithm
+function pow(a::Int, b::Int, c::Int) # hopefully faster than powermod
+    ans = 1
+    while b > 0
+        if b % 2 == 1
+            ans = mod(ans*a, c)
+            b -= 1
+        end
+        b = div(b, 2)
+        a = mod(a*a, c)
+    end
+    return ans
+end
 
-#n^17+1=(n+1)(n^2-n+1)(n^4-n^3+n^2-n+1)(n^8+n^7-n^5-n^4-n^3+n+1)
-# all primes divide each part of the polynomial in a cycle
-# ie 13 divides n(n-1)+1 when n = 4+13i or n = 10+13i
-
-function rem(a::Array, b::Array) # assume a[1] != 0 and b[1] = 1
-    tmp = copy(a)
-    denom = map(x -> 0-x, b[2:end])
-    for i in 1:(length(a)-length(denom))
-        for j in 1:length(denom)
-            tmp[i+j] += tmp[i]*denom[j]
+function primitiveroot(p::Int, primesl::Array{Int})
+    phi = p - 1
+    factors = primeFactorSet(phi, primesl)
+    for i in 1:phi
+        primitive = true
+        for f in factors
+            if pow(i, div(phi, f), p) == 1
+                primitive = false
+                break
+            end
+        end
+        if primitive
+            return i
         end
     end
-    return tmp[(length(tmp)-length(denom)+1):end]
 end
 
-function a1(p) # n+1
-    return Set{Int}([p-1])
+# (-x)^15 = 1 (mod p) <=> (x)^15 = -1 (mod p)
+function pthunity(p::Int, primesl::Array{Int})
+    d = gcd(15, p-1)
+    g = primitiveroot(p, primesl)
+    h = pow(g, div(p-1,d), p)
+    return sort([(p - pow(h, t, p)) for t in 0:(d-1)]) # (p-x)
 end
 
-function a2(p) # n(n-1)+1 = n^2-n+1
-    s = Set{Int}()
-    num = 0
-    for i in 1:p
-        if ((i*((p+1)-i)) % p) == 1
-            push!(s, i)
-            push!(s, (p+1)-i)
-            break
-        end
-    end
-    return s
-end
-
-function a22(p) # n(n-1)+1 = n^2-n+1
-    # BLEH
-    # BI always = zeros for primes that work (for some reason)
-    s = Set{Int}()
-    deg = 2
-    poly = [1,-1,1]
-    B = zeros(Int, (deg,deg))
-    B[1,1] = 1
-    for i in 1:(deg-1)
-        a = zeros(Int, (p*i))
-        unshift!(a, 1)
-        if length(a) >= length(poly)
-            a = rem(a, poly)
-        end
-        if length(a) != deg
-            prepend!(a, zeros(Int, deg-length(a)))
-        end
-        a = map(x -> mod(x, p), a)
-        B[i+1,1:end] = reverse(a)
-    end
-    BI = map(x -> mod(x, p),B - eye(B))
-    rank = 1 # compute this later
-    k = deg - rank
-    #kernel = Int[[1,1]]
-    println(B)
-    println(BI)
-    #println(kernel)
-    return s
-end
-
-println(a22(7))
-
-function a3(p) # n(n(n(n-1)+1)-1)+1 = n^4-n^3+n^2-n+1
-    s = Set{Int}()
-    num = 0
-    for i in 1:p
-        if (i*(i*(i*(BigInt(i)-1)+1)-1)+1) % p == 0
-            push!(s, i)
-            num += 1
-        end
-        if num == 4
-            break
-        end
-    end
-    return s
-end
-
-function a4(p) # n(n(n(n(n(n(n(n+1))-1)-1)-1))+1)+1 = n^8+n^7-n^5-n^4-n^3+n+1
-    s = Set{Int}()
-    num = 0
-    for i in 1:p
-        if (i*(i*(i*(i*(i*(i*(i*(BigInt(i)+1))-1)-1)-1))+1)+1) % p == 0
-            push!(s, i)
-            num += 1
-        end
-        if num == 8
-            break
-        end
-    end
-    return s
-end
-
-function S(n, m)
+function problem421(n, m)
     primesl = primes(m)
-    ans = BigInt(0)
-    s = Set{Int}()
+    ans = 0
     for p in primesl
-        if p == 2
-            s = Set{Int}([3])
-        else
-            s = a1(p)
-            s = union(s, a2(p))
-            #s = union(s, a3(p))
-            #s = union(s, a4(p))
-        end
-        for a in s
-            ans += p * div(n + (p-a), p)
+        ar = pthunity(p, primesl)
+        for a in ar
+            if n >= a
+                ans += p * (div(n-a, p) + 1)
+            end
         end
     end
     return ans
 end
 
-function S2(n, m)
-    primesl = primes(m)
-    ans = BigInt(0)
-    s = Set{Int}()
-    for p in primesl
-        if p == 2
-            s = Set{Int}([3])
-        else
-            s = a1(p)
-            s = union(s, a22(p))
-            #s = union(s, a3(p))
-            #s = union(s, a4(p))
-        end
-        for a in s
-            ans += p * div(n + (p-a), p)
-        end
-    end
-    return ans
-end
+println(problem421(10^11,10^3))
+Profile.init(10^8, 0.001)
+Profile.clear()
+@profile problem421(10^11,10^8)
+ProfileView.view()
 
-#println(S(BigInt(10)^11,10^4))
-#println(S2(BigInt(10)^11,10^4))
-
-#println(@benchmark S(BigInt(10)^11,10^3))
-#println(@benchmark S2(BigInt(10)^11,10^3))
+#println(@benchmark problem421(10^11,10^8))
 
